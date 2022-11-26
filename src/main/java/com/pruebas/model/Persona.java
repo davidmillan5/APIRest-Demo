8507b280759b220dfacc019afd1edfb290ca4eb8
Cesar David Millan <cesardavidmillan5@gmail.com>
2022-11-26 06:35:46 -0500

    API REST Demo

---
+++ src/main/java/com/pruebas/model/Persona.java
@@ -0,0 +1,59 @@
+package com.pruebas.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
+@Entity
+public class Persona {
+
+	@Id
+	private Integer id;
+	
+	@Column
+	private String nombre;
+	
+	@Column
+	private String apellido;
+	
+	@Column 
+	private Integer edad;
+
+	//Getters and Setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Integer getEdad() {
+		return edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+	
+	
+	
+	
+	
+}
